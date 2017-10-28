814032db542ac353a0fb29877e18e2a396bfc186
AdaZebra <rumusicmajor@yahoo.com>
2017-10-27 20:01:29 -0700

    More commenting updates

--- BISBASResults.js
+++ BISBASResults.js
@@ -1,16 +1,16 @@
-var BISPopulationMean =  19.99;
-var BISPopulationStd = 3.79;
-var BASRewardPopulationMean = 17.59;
-var BASRewardPopulationStd = 2.14;
-var BASDrviePopulationMean = 12.05;
-var BASDrviePopulationStd = 2.36;
-var BASFunPopulationMean = 12.43;
-var BASFunPopulationStd = 2.26;
-var BASOverallPopulationMean = 38.6;
-var BASOverallPopulationStd = 6.5;
+////////////////////////////////////////////////////////////
+//  This file was authored by A. Leah Zulas             ////
+//  It heavily utilizes D3 js                           ////
+//  If retreives data using Ajax, from an express server////
+//  Any questions about the below code should be sent to:///
+//  alzulas@alzulas.com                                 ////
+////////////////////////////////////////////////////////////
 
+
+//Create CSV array and retreive cookie
 var outPutCSV = [];
 var myid = getCookie("userName");
+//Use cookie to request data from the server, so long as cookie exists.
 if (myid != ""){
     var dataPassed;
     $.ajax({
@@ -18,9 +18,9 @@ if (myid != ""){
         url: "/BISresult/" + myid,
         async: false,
         success: function(dataPassed){
-            outPutCSV = calculateScores(dataPassed);
-            console.log(outPutCSV); //Correctly calculated data
-            console.log("Get request complete");
+            outPutCSV = calculateScores(dataPassed); //collect data and put them into the CSV
+            console.log(outPutCSV); //Correctly calculated data print to console so we can see it worked
+            console.log("Get request complete"); //verification that the data was retreieved.
         },
     });
 }
@@ -28,10 +28,14 @@ if (myid != ""){
 
 showBISResults(outPutCSV);
 showBASResults(outPutCSV);
-console.log(outPutCSV);//empty set
-createBargraph(outPutCSV);
+//console.log(outPutCSV);//empty set
+//createBargraph(outPutCSV);
 
-function createBargraph(dataset){
+function createBargraph(dataset){ //This was a test of D3
+    //It creates a little BISBAS graph
+    //It's not super useful
+    //But I kept the code just in case.
+    //If you'd like to see it, uncomment out the call to this function above.
     var w = 400;
     var h = 100;
     var barPadding = 1;
@@ -61,32 +65,35 @@ function createBargraph(dataset){
         });
 }
 
-function getCookie(cname) {
+function getCookie(cname) {//This function retreives the participant ID, stored as a cookie
     var name = cname + "=";
-    var ca = document.cookie.split(';');
+    var ca = document.cookie.split(';');//Ask for cookie, split by ;
     for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
+        if (c.indexOf(name) == 0) {//looking for ID in cookie called "cname"
+            return c.substring(name.length, c.length);//Return ID
         }
     }
-    return "";
+    return "";//No ID found, return empty string
 }
 
 function calculateScores(dataPassed){
+    //Takes data from CSV off the server, calculates relavant scores, returns array
     var BISScore = 0;
     var BASDriveScore = 0;
     var BASRewardScore = 0;
     var BASFunScore = 0;
     var scoresCSV = [];
-    var dataArray = dataPassed.split(",");
-    console.log(dataArray);
+    var dataArray = dataPassed.split(","); //Takes CSV from the server, creates array
+    //console.log(dataArray);
+    //Relevant data beings at position 5 in the array from the server, all BIS numbers are together, so we just add them up
     for (var i = 5; i < 12; i++){
         BISScore = Number(dataArray[i])+BISScore;
     }
+    //Same for the other scores. They are all together, so we add them up.
     for (var i = 12; i < 16; i++){
         BASDriveScore = Number(dataArray[i])+BASDriveScore;
     }
@@ -96,23 +103,31 @@ function calculateScores(dataPassed){
     for (var i = 21; i < 25; i++){
         BASFunScore = Number(dataArray[i])+BASFunScore;
     }
-    console.log(BISScore + " " + BASDriveScore + " " + BASRewardScore + " " + BASFunScore);
+    //console.log(BISScore + " " + BASDriveScore + " " + BASRewardScore + " " + BASFunScore);
+    //Push BIS and BAS scores onto a results array
     scoresCSV.push(BISScore);
     scoresCSV.push(BASDriveScore);
     scoresCSV.push(BASRewardScore);
     scoresCSV.push(BASFunScore);
-    console.log(scoresCSV);
+    
+    //return results to the ajax call
     return scoresCSV;
 }
 
 
 function showBISResults(outPutCSV){
+    //Important constants
+    var BISPopulationMean =  19.99;
+    var BISPopulationStd = 3.79;
+
+    //Some general text about what is to be displayed
     d3.select("body").append("H1").text("BIS Results")
     d3.select("body").append("p").text("The first temperament assessed was the Behavioral Inhibition System (BIS). This behavioral inhibition system involves a set of brain structures that leads to inhibition in response to anticipated or actual punishers and is associated with the experience of anxiety and fear.");
     d3.select("body").append("p").text("You can think of BIS as a psychic brake pedal, or a stop-and-throw-into-reverse pedal! BIS is activated for situations you perceive as novel, threatening, or challenging. People who score high on BIS experience anxiety/fear more readily and persistently in such situations.");
     d3.select("body").append("p").text("On the plus side, they are likely to be very careful and to consider consequences before acting and may be more systematic in thinking things through. On the down side, they may be more vulnerable to problems with anxiety.");
     d3.select("body").append("p").text("On this measure of behavioral inhibition, you scored a " + outPutCSV[0]);
 
+    //All text snippets needed for the next section.
     var veryHighBIS = "Your score for behavioral inhibition was in the Very High range. This means that compared to the average person, you are in the top 2% of people who experience anxiety more readily in response to novel, challenging, or threatening events.";
     var highBIS = "Your score for behavioral inhibition was in the High range. This means that compared to the average person, you are in the top 10% of people who experience anxiety more readily in response to novel, challenging, or threatening events.";
     var highAverageBIS = "Your score for behavioral inhibition was in the High Average range. This means that compared to the average person, you are in the top 25% of people who experience anxiety more readily in response to novel, challenging, or threatening events.";
@@ -121,7 +136,7 @@ function showBISResults(outPutCSV){
     var lowBIS = "Your score for behavioral inhibition was in the Low range. This means that compared to the average person, you are in the lower 10% of people who experience anxiety in response to novel, challenging, or threatening events.";
     var veryLowBIS = "Your score for behavioral inhibition was in the Very Low range. This means that compared to the average person, you are in the lower 2% of people who experience anxiety in response to novel, challenging, or threatening events.";
 
-
+    //Calculating which one to print by using the person's BIS score and finding which standard deviation they belong in. Then it prints out that text to the screen.
     if (outPutCSV[0] > BISPopulationMean-BISPopulationStd && outPutCSV[0] < BISPopulationMean+BISPopulationStd){
         d3.select("body").append("p").text(averageBIS);
     }
@@ -144,19 +159,33 @@ function showBISResults(outPutCSV){
         d3.select("body").append("p").text(veryHighBIS);
     }
     else{
+        //Something in the code failed most likely. But also, they could have not answered some of the questions.
         d3.select("body").append("p").text("Test was inconclusive or there is an error in the survey. Please contact the web developer.");
     }
 }
 
 function showBASResults(outPutCSV){
+    //Important constants
+    var BASRewardPopulationMean = 17.59;
+    var BASRewardPopulationStd = 2.14;
+    var BASDrviePopulationMean = 12.05;
+    var BASDrviePopulationStd = 2.36;
+    var BASFunPopulationMean = 12.43;
+    var BASFunPopulationStd = 2.26;
+    var BASOverallPopulationMean = 38.6;
+    var BASOverallPopulationStd = 6.5;
+
     var overallBAS = (outPutCSV[1]+outPutCSV[2]+outPutCSV[3]);
+    
+    //Some general text about what is to be displayed
     d3.select("body").append("H1").text("BAS Results");
     d3.select("body").append("p").text("The second temperament assessed was the Behavioral Activation System (BAS). The behavioral activation system involves a set of brain structures that promote movement toward incentives, things we want. You can think of BAS as a psychic gas pedal, a go system. BAS is activated for situations you want, situations that are enticing, attractive, positive. People who score high on BAS experience positive emotions more readily and persistently in such situations. On the plus side, they are likely to be motivated to approach positive situations with gusto. On the down side, they may have problems with being too impulsive.");
     d3.select("body").append("p").text("On this measure of behavioral activation, you scored " + overallBAS);
     d3.select("body").append("li").text("Your BAS Drive score is " + outPutCSV[1]);
     d3.select("body").append("li").text("Your BAS Reward score is " + outPutCSV[2]);
     d3.select("body").append("li").text("Your BAS Fun score is " + outPutCSV[3]);
-
+    
+    //All text snippets needed for the next section.
     var VeryHighBAS = "Your score for behavioral activation was in the Very High range. This means that compared to the average person, you are in the top 2% of people who experience positive emotion (e.g., excitement, enthusiasm) and motivation in response to positive events, such as rewards or incentives.";
     var highBAS = "Your score for behavioral activation was in the High range. This means that compared to the average person, you are in the top 10% of people who experience positive emotion (e.g., excitement, enthusiasm) and motivation in response to positive events, such as rewards or incentives.";
     var highAverageBAS = "Your score for behavioral activation was in the High Average range. This means that compared to the average person, you are in the top 25% of people who experience positive emotion (e.g., excitement, enthusiasm) and motivation in response to positive events, such as rewards or incentives.";
@@ -165,6 +194,7 @@ function showBASResults(outPutCSV){
     var lowBAS = "Your score for behavioral activation was in the Low range. This means that compared to the average person, you are in the lower 10% of people who experience positive emotion (e.g., excitement, enthusiasm) and motivation in response to positive events, such as rewards or incentives.";
     var veryLowBAS = "Your score for behavioral activation was in the Very Low range. This means that compared to the average person, you are in the lower 2% of people who experience positive emotion (e.g., excitement, enthusiasm) and motivation in response to positive events, such as rewards or incentives.";
     
+    //Calculating which one to print by using the person's BIS score and finding which standard deviation they belong in. Then it prints out that text to the screen.
     if (overallBAS > BASOverallPopulationMean-BASOverallPopulationStd && overallBAS < BASOverallPopulationMean+BASOverallPopulationStd){
         d3.select("body").append("p").text(averageBAS);
     }
@@ -187,6 +217,7 @@ function showBASResults(outPutCSV){
         d3.select("body").append("p").text(VeryHighBAS);
     }
     else{
+        //Something in the code failed most likely. But also, they could have not answered some of the questions.
         d3.select("body").append("p").text("Test was inconclusive or there is an error in the survey. Please contact the web developer.");
     }
 
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+////////////////////////////////////////////////////////////
+//  This file was authored by A. Leah Zulas             ////
+//  It creates an express server                        ////
+//  The server takes data after the servey is complete  ////
+//  It creates a CSV and then delivers data from that CSV///
+//      back to be visualized for the participant       ////
+//  Any questions about the below code should be sent to:///
+//  alzulas@alzulas.com                                 ////
+////////////////////////////////////////////////////////////
+
+//Important for setting up express
 var express        =         require("express");
 var bodyParser     =         require("body-parser");
 var app            =         express();
@@ -7,50 +18,34 @@ app.use(bodyParser.json());
 app.use(express.static('.'));
 
 
-//app.get('/RSSM.html',function(req,res){//sends data
-//  res.sendfile("RSSM.html");
-//});
-//app.get('/RSSM.js',function(req,res){
-//  res.sendfile("RSSM.js");
-//});
-//app.get('/BISBAS.html',function(req,res){//sends data
-//  res.sendfile("BISBAS.html");
-//});
-//app.get('/BISBAS.js',function(req,res){
-//  res.sendfile("BISBAS.js");
-//});
-//app.get('/BISBASResults.html',function(req,res){//takes data
-//  res.sendfile("BISBASResults.html");
-//});
-//app.get('/BISBASResults.js',function(req,res){
-//  res.sendfile("BISBASResults.js");
-//});
-//app.get('/RSSMResults.html',function(req,res){ //takes data
-//  res.sendfile("RSSMResults.html");
-//});
-//app.get('/RSSMResults.js',function(req,res){
-//  res.sendfile("RSSMResults.js");
-//});
-app.get('/BISresult/:myid', function (req, res) { //BISBASResults requests data like this
-  console.log(req.params.myid);
+//Requesting data to visualize BISBAS results
+app.get('/BISresult/:myid', function (req, res) { 
+  //console.log(req.params.myid);
   var myid = req.params.myid;
   var fs = require('fs');
   var IDfound = false;
   var currentLine = 0;
+  //read file
   var linesOfFile = fs.readFileSync('BISBAS.csv').toString().split("\n");
-  while (!IDfound){
+  while (!IDfound){ //find ID
     var tempLine = linesOfFile[currentLine];
     if (tempLine.includes(myid)){
         res.send(tempLine);
+        IDfound = true; //If ID is found, that's the line we want. Send it out.
+    }
+    else if (tempLine == ""){
         IDfound = true;
+        console.log("ID NOT FOUND");
     }
     else{
-        currentLine++;
+        currentLine++; //If not, then next line
     }
   } 
 })
+
+//Same as above
 app.get('/RSSMresult/:myid', function (req, res) {
-  console.log(req.params.myid);
+  //console.log(req.params.myid);
   var myid = req.params.myid;
   var fs = require('fs');
   var IDfound = false;
@@ -62,33 +57,46 @@ app.get('/RSSMresult/:myid', function (req, res) {
         res.send(tempLine);
         IDfound = true;
     }
+    else if (tempLine == ""){
+        IDfound = true;
+        console.log("ID NOT FOUND");
+    }
+
     else{
         currentLine++;
     }
   } 
 })
 
-
+//Take result of survey, put it into csv.
 app.post('/result',function(req,res){
   var surveyResult=req.body.survey;
   console.log("Results = "+JSON.stringify(surveyResult));
   res.end("yes");
-  turnToCSV(surveyResult);
+  turnToCSV(surveyResult); 
 });
+
+//server listens for requests on PORT 3000
 app.listen(3000,function(){
   console.log("Started on PORT 3000");
 })
 
+//Function to turn data from website into CSV 
 function turnToCSV(dataString){
+    //List needed to add which relationship each group of answers is
     var relationship = ["Mother", "Father", "Romantic Partner", "Ex-Romantic Partner", "Sibling", "Close Friend"];
     
+    //All of the various points we're trying to track for RSSM
     var RSSMHeadings =["ID","gender","age","race","employment","marritalStatus","relativeTyperel0","thoughtsrel0","competence1rel0","autonomy1rel0","relatedness1rel0","compentence2rel0","autonomy2rel0","relatedness2rel0","competentce3rel0","autonomy3rel0","relatedness3rel0","compentence4rel0","autonomy4rel0","relatedness4rel0","valencerel0","relativeTyperel1","thoughtsrel1","competence1rel1","autonomy1rel1","relatedness1rel1","compentence2rel1","autonomy2rel1","relatedness2rel1","competentce3rel1","autonomy3rel1","relatedness3rel1","compentence4rel1","autonomy4rel1","relatedness4rel1","valencerel1","relativeTyperel2","thoughtsrel2","competence1rel2","autonomy1rel2","relatedness1rel2","compentence2rel2","autonomy2rel2","relatedness2rel2","competentce3rel2","autonomy3rel2","relatedness3rel2","compentence4rel2","autonomy4rel2","relatedness4rel2","valencerel2","relativeTyperel3","thoughtsrel3","competence1rel3","autonomy1rel3","relatedness1rel3","compentence2rel3","autonomy2rel3","relatedness2rel3","competentce3rel3","autonomy3rel3","relatedness3rel3","compentence4rel3","autonomy4rel3","relatedness4rel3","valencerel3","relativeTyperel4","thoughtsrel4","competence1rel4","autonomy1rel4","relatedness1rel4","compentence2rel4","autonomy2rel4","relatedness2rel4","competentce3rel4","autonomy3rel4","relatedness3rel4","compentence4rel4","autonomy4rel4","relatedness4rel4","valencerel4","relativeTyperel5","thoughtsrel5","competence1rel5","autonomy1rel5","relatedness1rel5","compentence2rel5","autonomy2rel5","relatedness2rel5","competentce3rel5","autonomy3rel5","relatedness3rel5","compentence4rel5","autonomy4rel5","relatedness4rel5","valencerel5"];
 
+    //All data we are trying to track for BISBAS
     var BISBASHeadings = ["ID","gender","age","race","employment","BIS1","BIS2","BIS3","BIS4","BIS5","BIS6","BIS7","BASDrive1","BASDrive2","BASDrive3","BASDrive4","BASReward1","BASReward2","BASReward3","BASReward4","BASReward5","BASFun1","BASFun2","BASFun3","BASFun4"];
 
+    //turn JSON into a string
     var allDataAsString = JSON.stringify(dataString);
     //Data string must be parsed correctly.
     var dataHeadings;
+    //if it's BISBAS, use those headings, if RSSM, use those, Else call failure
     if (allDataAsString.includes("BIS")){
         dataHeadings = BISBASHeadings;
     }
@@ -98,23 +106,32 @@ function turnToCSV(dataString){
     else{
         console.log("FAILED TO LOAD DATA TYPES!");
     }
+    //Where am I in relationships
     var relationshipHeading = 0;
     var fileName;
     var csv = [];
+    //go through all the data headings we set earlier
     for (i = 0; i < dataHeadings.length; i++)
     {
+        //If the string includes the heading we are looking for
         if (allDataAsString.includes(dataHeadings[i])){
+            //Find the position of that heading
             var pos = allDataAsString.indexOf(dataHeadings[i]);
+            //Find the beginning of the data I want
             var beginPosition = pos+dataHeadings[i].length+2;
+            //If it's a bracket, look for the next bracket
             if (allDataAsString[beginPosition]=="["){
-                //console.log("bracket");
+                //put the first piece of the data into a string
                 var tempString = allDataAsString[beginPosition];
                 beginPosition++;
+                //Then do the entire string until I find the next bracket
                 while (allDataAsString[beginPosition] != "]"){
                     if (allDataAsString[beginPosition] == "\""){
                         tempString += "";
+                        //Get rid of JSON nonsense chars
                     }
                     else if (allDataAsString[beginPosition] == ","){
+                        //No commas in the string, because it creates a new piece in the excel file.
                         tempString += " & ";
                     }
                     else{
@@ -122,58 +139,69 @@ function turnToCSV(dataString){
                     }
                     beginPosition++;    
                 }
+                //add the final bracket and push onto the CSV array
                 tempString += "]";
-                console.log("brackets temp string: " + tempString);
                 csv.push(tempString);
             }
             else if (allDataAsString[beginPosition]=="\""){
-                //console.log("quotes");
+                //if the first character is a " save the string until the next "
                 var tempString = allDataAsString[++beginPosition];
                 beginPosition++;
                 while (allDataAsString[beginPosition] != "\""){
                     tempString += allDataAsString[beginPosition];
                     beginPosition++;
                 }
-                //tempString += "],";
-                console.log("quotes temp string: " + tempString);
+                //if the data is the ID, set that as the fileName
                 if (dataHeadings=="ID"){
                     fileName = tempString;
                 }
+                //Push onto CSV array
                 csv.push(tempString);
             }
             else{
+                //If the addition failed for any reason. 
                 console.log("ERROR IN DATA LOGGING");
             }
         }
         else if(dataHeadings[i].includes("relativeType")){
+            //if the string is a relative type, then push onto the array the relationship from the above list, and go next
             csv.push(relationship[relationshipHeading]);
             relationshipHeading++;
         }
         else{
+            //If none of the above, then the participant didn't answer that question, and push an empty string onto the spot.
             csv.push("");
         }
     }
+    //Push a return character, so that the next time we do this, the CSV will start a new line for the new participant
     csv.push("\n");
-    console.log(csv);
+    
     if (allDataAsString.includes("BIS")){
+        //If BIS BAS, put the array into the file BISBAS.csv
         var fs = require('fs');
         fs.appendFile("BISBAS.csv", csv, function(err) {
             if(err) {
+                //If failed, error
                 return console.log(err);
             }
+        //If not, return a saved log
         console.log("The file was saved!");
         });
     }
     else if (allDataAsString.includes("thoughts")){
+        //If RSSM, put the array into the file RSSM.csv
         var fs = require('fs');
         fs.appendFile("RSSM.csv", csv, function(err) {
             if(err) {
+                //If failed, error
                 return console.log(err);
             }
+        //If not, return a saved log
         console.log("The file was saved!");
         });
     }
     else{
+        //If anything goes wrong, fail.
         console.log("FAILED TO SAVE!");
     } 
 }
