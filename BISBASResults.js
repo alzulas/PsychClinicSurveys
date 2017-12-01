09561e8c06ff52fb87f2145066e23959da4ad59a
AdaZebra <rumusicmajor@yahoo.com>
2017-11-30 16:58:22 -0800

    graph test10

--- BISBASResults.js
+++ BISBASResults.js
@@ -40,37 +40,49 @@ function createBargraph(dataset){ //This was a test of D3
     //It's not super useful
     //But I kept the code just in case.
     //If you'd like to see it, uncomment out the call to this function above.
-    var w = 400;
-    var h = 100;
-    var barPadding = 1;
-
-    //Create SVG element
-    var svg = d3.select("body")
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h);
-
-    svg.selectAll("rect")
-        .data(dataset)
-        .enter()
-        .append("rect")
-        .attr("x", function(d, i) {
-            return i * (w / dataset.length);
-        })
-        .attr("y", function(d) {
-            return h - (d * 4);
-        })
-        .attr("width", w / dataset.length - barPadding)
-        .attr("height", function(d) {
-            return d * 4;
-        })
-        .attr("fill", function(d) {
-            return "rgb(0, 0, " + Math.round(d * 10) + ")";
-        })
-        .style("height", function(d) {
+			
+			d3.select("body").selectAll("div")
+				.data(dataset)
+				.enter()
+				.append("div")
+				.attr("class", "bar")
+				.style("height", function(d) {
 					var barHeight = d * 5;
 					return barHeight + "px";
 				});
+			
+		
+//    var w = 400;
+//    var h = 100;
+//    var barPadding = 1;
+//
+//    //Create SVG element
+//    var svg = d3.select("body")
+//                .append("svg")
+//                .attr("width", w)
+//                .attr("height", h);
+//
+//    svg.selectAll("rect")
+//        .data(dataset)
+//        .enter()
+//        .append("rect")
+//        .attr("x", function(d, i) {
+//            return i * (w / dataset.length);
+//        })
+//        .attr("y", function(d) {
+//            return h - (d * 4);
+//        })
+//        .attr("width", w / dataset.length - barPadding)
+//        .attr("height", function(d) {
+//            return d * 4;
+//        })
+//        .attr("fill", function(d) {
+//            return "rgb(0, 0, " + Math.round(d * 10) + ")";
+//        })
+//        .style("height", function(d) {
+//					var barHeight = d * 5;
+//					return barHeight + "px";
+//				});
 }
 
 function getCookie(cname) {//This function retreives the participant ID, stored as a cookie
