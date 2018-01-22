f46be889b00e66e6ba28c56ecfc9259dc2a89841
AdaZebra <rumusicmajor@yahoo.com>
2018-01-21 22:40:26 -0800

    Set textbox with ID

--- BISBASResults.js
+++ BISBASResults.js
@@ -37,7 +37,7 @@ function getCookie(cname) {//This function retreives the participant ID, stored 
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {//looking for ID in cookie called "cname"
-            document.getElementsByName('txtID').value = c.substring(name.length, c.length);
+            document.getElementById('txtID').value = c.substring(name.length, c.length);
             return c.substring(name.length, c.length);//Return ID
         }
     }
