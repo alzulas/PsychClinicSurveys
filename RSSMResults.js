0cc0d0e3c3936be3af820d6bfb14813bbe4ec079
AdaZebra <rumusicmajor@yahoo.com>
2018-01-26 14:42:30 -0800

    y change attempt

--- RSSMResults.js
+++ RSSMResults.js
@@ -254,7 +254,7 @@ function createBargraph(dataset){ //This was a test of D3
         .padding(0.05);
 
     var y = d3.scaleLinear()
-        .rangeRound([height, 0]);
+        .rangeRound([5, 0]);
 
     var z = d3.scaleOrdinal()
         .range(["#ccebff", "#66c2ff", "#007acc", "#005c99"]);
