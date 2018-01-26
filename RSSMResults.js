d5242198a90894897227841216b57e863d363eb1
AdaZebra <rumusicmajor@yahoo.com>
2018-01-25 23:24:46 -0800

    graph design, no data fixing

--- RSSMResults.js
+++ RSSMResults.js
@@ -220,119 +220,104 @@ function standardDeviationPrint(Score, Mean, SD, type) { //participants score, p
 
 function creatingBargraphData(dataset){
     //Relationship,Competance,autonomy,relatedness
-    var newCSV = [["Relationship","Competance","Autonomy","Relatedness"],
-        [relationship[0],dataset[0],dataset[1],dataset[2]],
-        [relationship[1],dataset[3],dataset[4],dataset[5]],
-        [relationship[2],dataset[6],dataset[7],dataset[8]],
-        [relationship[3],dataset[9],dataset[10],dataset[11]],
-        [relationship[4],dataset[12],dataset[13],dataset[14]]
-                 ];
     
-    var fs = require('fs');
-        
+    var newCSV=[];
+    
+    var i = 0;
     for(j = 0; j < relationship.length; j++){
-        for(i = 0; i < dataset.length; i++){
-            line = relationship[j] + "," + dataset[i] + "," + dataset[i+1] + "," + dataset[i+2] + '\n';
-            i = i+2;
-            fs.appendFile("graph.csv", line, function(err) {
-            if(err) {
-                //If failed, error
-                return console.log(err);
-            }
-                //If not, return a saved log
-                console.log("The file was saved!");
-
-            });
-        }
+            var line = {Relationship: relationship[j],Competance: dataset[i],Autonomy: dataset[i+1],Relatedness: dataset[i+2]};
+            i = i+3;
+            newCSV.push(line);
+            //console.log(line);
     }
-        
-    
-    
-       
+    //console.log(newCSV);
+             
 
-var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select("svg"),
+        margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom,
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var x0 = d3.scaleBand()
-    .rangeRound([0, width])
-    .paddingInner(0.1);
+    var x0 = d3.scaleBand()
+        .rangeRound([0, width])
+        .paddingInner(0.1);
 
-var x1 = d3.scaleBand()
-    .padding(0.05);
+    var x1 = d3.scaleBand()
+        .padding(0.05);
 
-var y = d3.scaleLinear()
-    .rangeRound([height, 0]);
+    var y = d3.scaleLinear()
+        .rangeRound([height, 0]);
 
-var z = d3.scaleOrdinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    var z = d3.scaleOrdinal()
+        .range(["#ccebff", "#66c2ff", "#007acc", "#005c99"]);
 
-d3.csv("data.csv", function(d, i, columns) {
-  for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]];
-  return d;
-}, function(error, data) {
-  if (error) throw error;
+    d3.csv("data1.csv", function(d, i, columns) {
+      for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]];
+      return d;
+    }, function(error, data) {
+      if (error) throw error;
+      console.log(data);
+      //var data = newCSV;
 
-  var keys = data.columns.slice(1);
+      var keys = data.columns.slice(1);
 
-  x0.domain(data.map(function(d) { return d.Relationship; }));
-  x1.domain(keys).rangeRound([0, x0.bandwidth()]);
-  y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
+      x0.domain(data.map(function(d) { return d.Relationship; }));
+      x1.domain(keys).rangeRound([0, x0.bandwidth()]);
+      y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
 
-  g.append("g")
-    .selectAll("g")
-    .data(data)
-    .enter().append("g")
-      .attr("transform", function(d) { return "translate(" + x0(d.Relationship) + ",0)"; })
-    .selectAll("rect")
-    .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
-    .enter().append("rect")
-      .attr("x", function(d) { return x1(d.key); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("width", x1.bandwidth())
-      .attr("height", function(d) { return height - y(d.value); })
-      .attr("fill", function(d) { return z(d.key); });
+      g.append("g")
+        .selectAll("g")
+        .data(data)
+        .enter().append("g")
+          .attr("transform", function(d) { return "translate(" + x0(d.Relationship) + ",0)"; })
+        .selectAll("rect")
+        .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
+        .enter().append("rect")
+          .attr("x", function(d) { return x1(d.key); })
+          .attr("y", function(d) { return y(d.value); })
+          .attr("width", x1.bandwidth())
+          .attr("height", function(d) { return height - y(d.value); })
+          .attr("fill", function(d) { return z(d.key); });
 
-  g.append("g")
-      .attr("class", "axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x0));
+      g.append("g")
+          .attr("class", "axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x0));
 
-  g.append("g")
-      .attr("class", "axis")
-      .call(d3.axisLeft(y).ticks(null, "s"))
-    .append("text")
-      .attr("x", 2)
-      .attr("y", y(y.ticks().pop()) + 0.5)
-      .attr("dy", "0.32em")
-      .attr("fill", "#000")
-      .attr("font-weight", "bold")
-      .attr("text-anchor", "start")
-      .text("Population");
+      g.append("g")
+          .attr("class", "axis")
+          .call(d3.axisLeft(y).ticks(null, "s"))
+        .append("text")
+          .attr("x", 2)
+          .attr("y", y(y.ticks().pop()) + 0.5)
+          .attr("dy", "0.32em")
+          .attr("fill", "#000")
+          .attr("font-weight", "bold")
+          .attr("text-anchor", "start")
+          .text("Population");
 
-  var legend = g.append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .attr("text-anchor", "end")
-    .selectAll("g")
-    .data(keys.slice().reverse())
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+      var legend = g.append("g")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", 10)
+          .attr("text-anchor", "end")
+        .selectAll("g")
+        .data(keys.slice().reverse())
+        .enter().append("g")
+          .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  legend.append("rect")
-      .attr("x", width - 19)
-      .attr("width", 19)
-      .attr("height", 19)
-      .attr("fill", z);
+      legend.append("rect")
+          .attr("x", width - 19)
+          .attr("width", 19)
+          .attr("height", 19)
+          .attr("fill", z);
 
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9.5)
-      .attr("dy", "0.32em")
-      .text(function(d) { return d; });
-});
+      legend.append("text")
+          .attr("x", width - 24)
+          .attr("y", 9.5)
+          .attr("dy", "0.32em")
+          .text(function(d) { return d; });
+    });
 
 
 //    for (var j = 0; i < relationship.length; j++){
